4eb95cbea6f970397ce74256cd6874441f9615b9
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2023-07-29 10:16:20 +0100

    monkeyed in the JL header to Tiers

--- demo/project.py
+++ demo/project.py
@@ -1,4 +1,19 @@
 from cassini import Project, DEFAULT_TIERS
+from IPython.display import display
+
+
+# Hacking in the jl gui.
+class JLGui:
+
+    def __init__(self, tier):
+        self.tier = tier
+        
+    def header(self):
+        display({'application/cassini.header+json': {}}, raw=True)
+
+
+for Tier in DEFAULT_TIERS:
+    Tier.gui_cls = JLGui
 
 project = Project(DEFAULT_TIERS, __file__)
 
