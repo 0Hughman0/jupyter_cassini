29a245908e21afa4487a3e15d2e296b640dfb585
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2023-08-09 18:48:44 +0100

    removed old boilerplate, hopefully fixing installation issues

--- jupyter_cassini_server/__init__.py
+++ jupyter_cassini_server/__init__.py
@@ -4,20 +4,14 @@ from pathlib import Path
 
 from ._version import __version__
 
-HERE = Path(__file__).parent.resolve()
+from .handlers import setup_handlers
 
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
 
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": data["name"]
+        "dest": "jupyter_cassini"
     }]
-
-
-
-from .handlers import setup_handlers
 
 
 def _jupyter_server_extension_points():
