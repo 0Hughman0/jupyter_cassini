31845dd760e99175dfdcdfeb4f2865c48d5e8643
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2023-08-09 21:07:40 +0100

    improved logging in integration tests

--- ui-tests/test_project.py
+++ ui-tests/test_project.py
@@ -1,33 +1,27 @@
 from cassini import Project, DEFAULT_TIERS
+from cassini.jlgui import extend_project
 
 import shutil
 from tempfile import mkdtemp
 import os
 
-from IPython.display import display
-
+print("First")
 project_folder = os.environ.get('JUPYTERLAB_GALATA_ROOT_DIR')
+print(os.environ.get('JUPYTERLAB_GALATA_ROOT_DIR'))
 
 if project_folder is None:
     project_folder = mkdtemp(prefix='cassini-testing')
     os.environ['JUPYTERLAB_GALATA_ROOT_DIR'] = project_folder
-    
-    shutil.copy(__file__, os.path.join(project_folder, 'project.py')) # put the project.py into the temp folder.
 
-class JLGui:
+    print("Made test directory", project_folder)
+    print("Set to JUPYTERLAB_GALATA_ROOT_DIR End")
 
-    def __init__(self, tier):
-        self.tier = tier
-        
-    def header(self):
-        display({'application/cassini.header+json': {}}, raw=True)
-
-
-for Tier in DEFAULT_TIERS:
-    Tier.gui_cls = JLGui
+    print("Moving in template project file")
+    shutil.copy(__file__, os.path.join(project_folder, 'project.py'))  # put the project.py into the temp folder.
+    print("Done")
 
 project = Project(DEFAULT_TIERS, project_folder)
-
+extend_project(project)
 
 
 if __name__ == '__main__':
