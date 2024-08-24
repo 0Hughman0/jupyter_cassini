cf72d0dc3320588c7208e25590a2a613d72cbdbf
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2024-08-24 15:35:33 +0100

    updated demo files

--- demo/cas_project.py
+++ demo/cas_project.py
@@ -1,6 +1,8 @@
 from cassini import Project, DEFAULT_TIERS
+from cassini.ext import cassini_lib
 
 project = Project(DEFAULT_TIERS, __file__)
+project = cassini_lib.extend_project(project)
 
 if __name__ == '__main__':
     project.launch()
