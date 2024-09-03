5d74fc40101c0d671c5054e85a856a20179c85e4
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2024-09-03 17:22:26 +0100

    stopped using cas_lib extension in tests

--- jupyter_cassini_server/tests/project/cas_project.py
+++ jupyter_cassini_server/tests/project/cas_project.py
@@ -1,8 +1,6 @@
 from cassini import DEFAULT_TIERS, Project
-from cassini.ext import cassini_lib
 
 project = Project(DEFAULT_TIERS, __file__)
-project = cassini_lib.extend_project(project)
 
 if __name__ == '__main__':
     project.launch()
