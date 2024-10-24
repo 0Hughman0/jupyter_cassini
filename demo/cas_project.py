b621b953fb9f8a38154d8ef981d15a5b4c8ddb96
Hugh Ramsden <hugh.o.ramsden@gmail.com>
2024-10-24 15:39:33 +0100

    fixed non-browser compat svg

--- demo/cas_project.py
+++ demo/cas_project.py
@@ -2,20 +2,18 @@ import datetime
 from typing import Literal
 
 from cassini import Project, DEFAULT_TIERS, WorkPackage
-from cassini.meta import MetaManager
+from cassini.meta import MetaAttr
 from cassini.ext import cassini_lib
 from pydantic import SecretStr
 
-manager = MetaManager()
 
-@manager.connect_class
 class MyWP(WorkPackage):
     name_part_template = 'WP{}'
     pretty_type = 'WorkPackage'
-    a_datetime = manager.meta_attr(datetime.datetime, datetime.datetime)
-    a_date = manager.meta_attr(datetime.date, datetime.date)
-    one_of = manager.meta_attr(Literal['bees', 'fish'], str)
-    an_int = manager.meta_attr(int, int)
+    a_datetime = MetaAttr(datetime.datetime, datetime.datetime)
+    a_date = MetaAttr(datetime.date, datetime.date)
+    one_of = MetaAttr(Literal['bees', 'fish'], str)
+    an_int = MetaAttr(int, int)
 
 project = Project([DEFAULT_TIERS[0], MyWP, *DEFAULT_TIERS[2:]], __file__)
 project = cassini_lib.extend_project(project)
